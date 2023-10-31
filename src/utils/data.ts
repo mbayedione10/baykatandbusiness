7a0551fb38caa6b48a5ea0551bc7d4a19d3120b7
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2023-10-31 15:39:09 +0000

    ✨ feat (Services): refactore component and add data

---
+++ src/utils/data.ts
@@ -0,0 +1,55 @@
+const services = [
+    {
+        title: "Services phytosanitaires",
+        description: `
+       • Diagnostic à distance ou sur site
+       • Plan de traitement phytosanitaire
+       • Intervention sur site
+    `
+    },
+    {
+        title: "Optimisation sol",
+        description: `
+       • Diagnostic état du terrain agricole
+       • Plan de fertilisation ou de décontamination
+       • Intervention sur site
+    `
+    },
+    {
+        title: "Planification agricole",
+        description: `
+       • Elaboration plan d'investissement
+       • Elaboration calendrier cultural sur 2 ans
+       • Elaboration comptes d'exploitation
+    `
+    },
+    {
+        title: "Gestion irrigation",
+        description: `
+       • Prise d'informations pédoclimatiques
+       • Plan d'extration d'eau
+       • Dimensionnement réseau d'irrigation
+       • Installation réseau d'irrigation
+    `
+    },
+    {
+        title: "Formations",
+        description: `
+       • Formation théorique PME en Agrobusiness
+       • Formation personnel technique
+       • Formation d'initiation aux pratiques agricoles au grand public
+    `
+    },
+    {
+        title: "Distribution",
+        description: `
+       • Plateforme web et application mobile
+       • Réseaux de partenaires
+       • Publicité en ligne
+       • Réseau de points focaux
+    `
+    },
+    
+]
+
+export { services }
